5b337d0eb233331e0a0dbec7dcbe905eea738ef3
Toby Francis <151694080+Frisamr@users.noreply.github.com>
2026-05-13 15:07:18 +1000

    haskell

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -133,6 +133,7 @@ return {
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
 
+        hls = {}, -- haskell language server
         stylua = {}, -- Used to format Lua code
 
         -- Special Lua Config, as recommended by neovim help docs
